286db871db12b9206b3f2baa4217bb974adf5861
equasoda <haachch@gmail.com>
2019-06-03 16:53:06 +0800

    完成我的页面排版

--- ele/src/App.vue
+++ ele/src/App.vue
@@ -31,20 +31,16 @@ export default {
 </script>
 
 <style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
+@import './font/iconfont.wxss';
+  *{
+    margin:0;
+    padding:0;
+    list-style: none;
+    box-sizing: border-box;
+  }
+  page{
+    width:100%;
+    height:100%;
+  }
+
 </style>
---
+++ ele/src/pages/tab/my/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="my">
+    <div class="profile-number">
+      <div class="avator">
+        <span class="iconfont icon-morentouxiang"></span>
+      </div>
+      <div class="login">
+        <div class="user-info">
+          <p>登录/注册</p>
+          <p>
+            <span class="iconfont icon-shouji"></span>暂无绑定手机号
+          </p>
+        </div>
+        <span class="iconfont icon-youjiantou"></span>
+      </div>
+    </div>
+    <div class="info-data">
+      <li>
+        <span>
+          <b>0.00</b>
+          <i>元</i>
+        </span>
+        <span>我的余额</span>
+      </li>
+      <li>
+        <span>
+          <b>0</b>
+          <i>个</i>
+        </span>
+        <span>我的优惠</span>
+      </li>
+      <li>
+        <span>
+          <b>0</b>
+          <i>分</i>
+        </span>
+        <span>我的积分</span>
+      </li>
+    </div>
+    <div class="info-order">
+      <ul>
+        <li>
+          <span class="iconfont icon-dingdan"></span>
+          <span class="second">我的订单</span>
+        </li>
+        <span class="iconfont icon-youjiantou"></span>
+      </ul>
+      <ul>
+        <li>
+          <span class="iconfont icon-jifentixicopy"></span>
+          <span class="second">积分商城</span>
+        </li>
+        <span class="iconfont icon-youjiantou"></span>
+      </ul>
+      <ul>
+        <li>
+          <span class="iconfont icon-icon_huangguan"></span>
+          <span class="second">饿了么会员卡</span>
+        </li>
+        <span class="iconfont icon-youjiantou"></span>
+      </ul>
+    </div>
+    <div class="info-server">
+      <ul>
+        <li>
+          <span class="iconfont icon-fuwu"></span>
+          <span class="second">服务中心</span>
+        </li>
+        <span class="iconfont icon-youjiantou"></span>
+      </ul>
+      <ul>
+        <li>
+          <span class="iconfont icon-changyonglogo40"></span>
+          <span class="second">下载饿了么APP</span>
+        </li>
+        <span class="iconfont icon-youjiantou"></span>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped lang="scss">
+  @import "./my.scss";
+</style>
+
+
